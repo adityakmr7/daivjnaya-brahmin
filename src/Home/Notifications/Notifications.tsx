8722923c41ca43362638bbca1e5263e188dd6ebf
Aditya kumar <adityakmr9672@gmail.com>
2020-09-19 22:48:06 +0530

    scrollevent added in notification screen

--- src/Home/Notifications/Notifications.tsx
+++ src/Home/Notifications/Notifications.tsx
@@ -12,34 +12,42 @@ const Notifications = ({}: NotificationsProps) => {
   const CARD_HEIGHT = wWidth;
   return (
     <Box flex={1} backgroundColor="mainBackground">
-      <Box
-        backgroundColor="iconBackground"
-        elevation={1}
-        borderRadius="s"
-        marginTop="xl"
-        marginBottom="l"
-        height={CARD_HEIGHT}
-        width={CARD_WIDTH}
-        marginHorizontal="l"
-        // backgroundColor="notificationColor"
-      >
-        <Box>
-          <Image
-            style={{ width: CARD_WIDTH, borderRadius: 6 }}
-            source={require("./assets/card-image.png")}
-          />
-        </Box>
-        <Box marginVertical="s" marginHorizontal="s">
-          <Text color="primaryText" variant="silentText">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui
-            blanditiis
-          </Text>
-          <Text marginVertical="s" variant="mainIconSubTitle">
-            Morbi a suscipit ipsum. Suspendisse mollis libero ante. Pellentesque
-            finibus convallis nulla vel placerat. Nulla ipsum…
-          </Text>
-        </Box>
-      </Box>
+      <ScrollView showsHorizontalScrollIndicator={false} horizontal={true}>
+        {[1, 2, 3].map((_, index) => {
+          return (
+            <Box
+              key={index}
+              backgroundColor="iconBackground"
+              elevation={1}
+              borderRadius="s"
+              marginTop="xl"
+              marginBottom="l"
+              height={CARD_HEIGHT}
+              width={CARD_WIDTH}
+              marginLeft="l"
+            >
+              <Box>
+                <Image
+                  style={{ width: CARD_WIDTH, borderRadius: 6 }}
+                  source={require("./assets/card-image.png")}
+                />
+              </Box>
+              <Box marginVertical="s" marginHorizontal="s">
+                <Text color="primaryText" variant="silentText">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis
+                </Text>
+                <Text marginVertical="s" variant="mainIconSubTitle">
+                  Morbi a suscipit ipsum. Suspendisse mollis libero ante.
+                  Pellentesque finibus convallis nulla vel placerat. Nulla
+                  ipsum…
+                </Text>
+              </Box>
+            </Box>
+          );
+        })}
+      </ScrollView>
+
       <SectionHeader title="Videos" onPress={() => {}} />
       <Box
         paddingVertical="l"
