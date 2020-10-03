613933c4cab0a462d12b9fc7ed15b5da5cc1a6db
Aditya kumar <adityakmr9672@gmail.com>
2020-10-03 13:49:05 +0530

    vendor and bride added

--- src/Screens/Matrimony/Bride.tsx
+++ src/Screens/Matrimony/Bride.tsx
@@ -1,12 +1,72 @@
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
 import { Box, Text } from "../../components";
+import HorizontalCard from "../communityHub/components/HorizontalCard";
+import { MatrimonyRootParamList } from "./MatrimonyRoutes";
 
 interface BrideProps {}
-const Bride = ({}: BrideProps) => {
+
+const BrideDetail = [
+  {
+    id: 1,
+    title: "Full Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod…",
+    image: require("./assets/bride-1.png"),
+    btn: "View full details",
+  },
+  {
+    id: 2,
+    title: "Full Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod…",
+    image: require("./assets/bride-2.png"),
+    btn: "View full details",
+  },
+  {
+    id: 3,
+    title: "Full Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod…",
+    image: require("./assets/bride-3.png"),
+    btn: "View full details",
+  },
+  {
+    id: 3,
+    title: "Full Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod…",
+    image: require("./assets/bride-4.png"),
+    btn: "View full details",
+  },
+];
+
+const Bride = ({
+  navigation,
+}: {
+  navigation: BottomTabNavigationProp<MatrimonyRootParamList, "Bride">;
+}) => {
   return (
-    <Box>
-      <Text>Bride</Text>
-    </Box>
+    <ScrollView>
+      <Box backgroundColor="iconBackground" flex={1}>
+        <Box>
+          {BrideDetail.map((data, i) => {
+            return (
+              <HorizontalCard
+                key={i}
+                onPress={() =>
+                  navigation.navigate("GroomDetail", {
+                    id: data.id,
+                  })
+                }
+                {...{ data }}
+              />
+            );
+          })}
+        </Box>
+      </Box>
+    </ScrollView>
   );
 };
 
--- src/Screens/Matrimony/Vendors.tsx
+++ src/Screens/Matrimony/Vendors.tsx
@@ -1,12 +1,71 @@
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import React from "react";
+import { ScrollView } from "react-native-gesture-handler";
 import { Box, Text } from "../../components";
+import HorizontalCard from "../communityHub/components/HorizontalCard";
+import { MatrimonyRootParamList } from "./MatrimonyRoutes";
 
 interface VendorsProps {}
-const Vendors = ({}: VendorsProps) => {
+const VendorDetail = [
+  {
+    id: 1,
+    title: "Full Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod…",
+    image: require("./assets/vendor-1.png"),
+    btn: "View full details",
+  },
+  {
+    id: 2,
+    title: "Full Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod…",
+    image: require("./assets/vendor-2.png"),
+    btn: "View full details",
+  },
+  {
+    id: 3,
+    title: "Full Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod…",
+    image: require("./assets/vendor-3.png"),
+    btn: "View full details",
+  },
+  {
+    id: 3,
+    title: "Full Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod…",
+    image: require("./assets/vendor-4.png"),
+    btn: "View full details",
+  },
+];
+
+const Vendors = ({
+  navigation,
+}: {
+  navigation: BottomTabNavigationProp<MatrimonyRootParamList, "Vendors">;
+}) => {
   return (
-    <Box>
-      <Text>Vendors</Text>
-    </Box>
+    <ScrollView>
+      <Box backgroundColor="iconBackground" flex={1}>
+        <Box>
+          {VendorDetail.map((data, i) => {
+            return (
+              <HorizontalCard
+                key={i}
+                onPress={() =>
+                  navigation.navigate("GroomDetail", {
+                    id: data.id,
+                  })
+                }
+                {...{ data }}
+              />
+            );
+          })}
+        </Box>
+      </Box>
+    </ScrollView>
   );
 };
 
