6ce805d7a822f03a1687dd53ceb8c44c9eb78d6d
Aditya kumar <adityakmr9672@gmail.com>
2020-11-01 11:14:56 +0530

    redux logger added

--- src/store.tsx
+++ src/store.tsx
@@ -3,8 +3,11 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+
+const middleware = [thunk, logger];
 
 export const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(...middleware))
 );
