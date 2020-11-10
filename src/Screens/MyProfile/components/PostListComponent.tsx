b3bfe907afe640ca22be66c1772355291e27a176
Aditya kumar <adityakmr9672@gmail.com>
2020-11-10 19:56:28 +0530

    postliked added

--- src/Screens/MyProfile/components/PostListComponent.tsx
+++ src/Screens/MyProfile/components/PostListComponent.tsx
@@ -9,16 +9,19 @@ interface PostListComponentProps {
   postList: any;
   onPress: () => void;
   userProfileData: any;
+  makePostLike: (pId: number) => void;
 }
 const PostListComponent = ({
   postList,
   userProfileData,
+  makePostLike,
   onPress,
 }: PostListComponentProps) => {
   const { _embedded } = postList;
   const handlePostLikeDisLike = (pId: number) => {
     // TODO: handle post like dislike here
-    console.log("post id", pId);
+
+    makePostLike(pId);
   };
   //TODO: use FLatList here
   return (
@@ -46,4 +49,14 @@ const PostListComponent = ({
     </Box>
   );
 };
-export default PostListComponent;
+
+function mapStateToProps(state: any) {
+  return {
+    ...state,
+  };
+}
+
+const mapDispatchToProps = (dispatch: any) => ({
+  makePostLike: (pId: number) => dispatch(postIdPostLike(pId)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(PostListComponent);
