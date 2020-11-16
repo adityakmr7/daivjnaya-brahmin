d91f6e42d5098decd5ee1622b6fab2c5d8d7aae9
Aditya kumar <adityakmr9672@gmail.com>
2020-11-16 12:05:29 +0530

    signup dropdown added

--- src/Screens/Authentication/SignUp.tsx
+++ src/Screens/Authentication/SignUp.tsx
@@ -10,6 +10,7 @@ import { connect } from "react-redux";
 import { useToast } from "react-native-styled-toast";
 
 import { AuthState } from "../../reducers/authReducer";
+import { Picker } from "@react-native-community/picker";
 const { width: wWidth, height: wHeight } = Dimensions.get("window");
 
 interface SignupProps {
@@ -24,6 +25,119 @@ interface SignupProps {
     navigation: any
   ) => void;
 }
+const lastNameList = [
+  { id: 1, lName: "Achrekar" },
+  { id: 2, lName: "Anvekar" },
+  { id: 3, lName: "Amrute" },
+  { id: 4, lName: "Arkashali" },
+  { id: 5, lName: "Aryamane" },
+  { id: 6, lName: "Baikerikar" },
+  { id: 7, lName: "Bandivadekar" },
+  { id: 8, lName: "Bandodkar" },
+  { id: 9, lName: "Belekar" },
+  { id: 10, lName: "Belvalkar" },
+  { id: 11, lName: "Bhadekar" },
+  { id: 12, lName: "Bhatt" },
+  { id: 13, lName: "Bhide" },
+  { id: 14, lName: "Borkar" },
+  { id: 15, lName: "Chachad" },
+  { id: 16, lName: "Chandgadkar" },
+  { id: 17, lName: "Chapwale" },
+  { id: 18, lName: "Chindarkar" },
+  { id: 19, lName: "Chiplunkar" },
+  { id: 20, lName: "Chodankar" },
+  { id: 21, lName: "Daivajna" },
+  { id: 22, lName: "Devrukhkar or Deorukhkar" },
+  { id: 23, lName: "Dhargalkar" },
+  { id: 24, lName: "Dhotre" },
+  { id: 25, lName: "Gangolli" },
+  { id: 26, lName: "Ghosalkar" },
+  { id: 27, lName: "Guhagarkar" },
+  { id: 28, lName: "Gujrathi" },
+  { id: 29, lName: "Haldankar" },
+  { id: 30, lName: "Hate" },
+  { id: 31, lName: "Hatkar" },
+  { id: 32, lName: "Herekar" },
+  { id: 33, lName: "Jamsandekar" },
+  { id: 34, lName: "Jannu" },
+  { id: 35, lName: "Jathar" },
+  { id: 36, lName: "Kagalkar" },
+  { id: 37, lName: "Kannikar" },
+  { id: 38, lName: "Kale" },
+  { id: 39, lName: "Kagalka" },
+  { id: 40, lName: "Karekar" },
+  { id: 41, lName: "Kerkar" },
+  { id: 42, lName: "Khandeparkar" },
+  { id: 43, lName: "Khedekar" },
+  { id: 44, lName: "Kolvekar" },
+  { id: 45, lName: "Kshirsagar" },
+  { id: 46, lName: "Kudtarkar" },
+  { id: 47, lName: "Kulkarni" },
+  { id: 48, lName: "Kundpura" },
+  { id: 49, lName: "Kurdekar" },
+  { id: 50, lName: "Kuste" },
+  { id: 51, lName: "Lotlikar" },
+  { id: 52, lName: "Madkaikar" },
+  { id: 53, lName: "Maldikar" },
+  { id: 54, lName: "Malandkar" },
+  { id: 55, lName: "Malekar" },
+  { id: 56, lName: "Malondkar" },
+  { id: 57, lName: "Malpekar" },
+  { id: 58, lName: "Mankame" },
+  { id: 59, lName: "Mhashelkar" },
+  { id: 60, lName: "Mhashilkar" },
+  { id: 61, lName: "Mislankar" },
+  { id: 62, lName: "Mitkar" },
+  { id: 63, lName: "Mohare" },
+  { id: 64, lName: "Murkute" },
+  { id: 65, lName: "Pednekar" },
+  { id: 66, lName: "Pawaskar" },
+  { id: 67, lName: "Pauskar" },
+  { id: 68, lName: "Paudwal" },
+  { id: 69, lName: "Patkar" },
+  { id: 70, lName: "Palshetkar" },
+  { id: 71, lName: "Palankar" },
+  { id: 72, lName: "Paithankar" },
+  { id: 73, lName: "Nerurkar" },
+  { id: 74, lName: "Nesrikar" },
+  { id: 75, lName: "Navghare" },
+  { id: 76, lName: "Narvekar" },
+  { id: 77, lName: "Narkar" },
+  { id: 78, lName: "Nagvekar" },
+  { id: 79, lName: "Shankarsheth" },
+  { id: 80, lName: "Sawkar" },
+  { id: 81, lName: "Salkar" },
+  { id: 82, lName: "Satvilkar" },
+  { id: 83, lName: "Saitavdekar" },
+  { id: 84, lName: "Sanu" },
+  { id: 85, lName: "Sambhare" },
+  { id: 86, lName: "Rajapurkar" },
+  { id: 87, lName: "Revankar" },
+  { id: 88, lName: "Rao" },
+  { id: 89, lName: "Ramamurthy" },
+  { id: 90, lName: "Raikar" },
+  { id: 91, lName: "Prabhulikar" },
+  { id: 92, lName: "Powale" },
+  { id: 93, lName: "Potdar" },
+  { id: 94, lName: "Pitale" },
+  { id: 95, lName: "Pendurkar" },
+  { id: 96, lName: "Shet" },
+  { id: 97, lName: "Shejwadkar" },
+  { id: 98, lName: "Shejekan" },
+  { id: 99, lName: "Verlekar" },
+  { id: 100, lName: "Vedpathak" },
+  { id: 101, lName: "Vedak" },
+  { id: 102, lName: "Vaiude" },
+  { id: 103, lName: "Vaidya" },
+  { id: 104, lName: "Vagal" },
+  { id: 105, lName: "Usapkar" },
+  { id: 106, lName: "Umrotkar" },
+  { id: 107, lName: "Shirodkar" },
+  { id: 108, lName: "Vernekar" },
+  { id: 109, lName: "Vesnekar" },
+  { id: 110, lName: "Vithalkar" },
+  { id: 111, lName: "Wagal" },
+];
 
 const validationSchema = Yup.object().shape({
   password: Yup.string()
@@ -32,7 +146,7 @@ const validationSchema = Yup.object().shape({
     )
     .required(),
   email: Yup.string().email().required(),
-  fName: Yup.string().required(),
+  firstName: Yup.string().required(),
   phoneNumber: Yup.string().length(10).required(),
 });
 
@@ -50,26 +164,21 @@ const SignUp = ({ navigation, userSignUp, userSignupState }: SignupProps) => {
   } = useFormik({
     validationSchema,
     initialValues: {
+      firstName: "",
+      lastName: "",
       password: "",
-      fName: "",
       email: "",
       phoneNumber: "",
       callback: false,
     },
     onSubmit: async (values) => {
-      const { email, password, fName, phoneNumber } = values;
-      const fullName = fName.split(" ");
-      const firstName = fullName[0];
-      const lastName = fullName[1];
+      const { email, password, firstName, lastName, phoneNumber } = values;
+      // const fullName = fName.split(" ");
+      const fName = firstName;
+      const lName = lastName;
+      console.log({ values });
       if (values.callback === true) {
-        userSignUp(
-          email,
-          password,
-          firstName,
-          lastName,
-          phoneNumber,
-          navigation
-        );
+        userSignUp(email, password, fName, lName, phoneNumber, navigation);
       } else {
         toast({
           message: "Accept Terms and Conditions",
@@ -141,13 +250,40 @@ const SignUp = ({ navigation, userSignUp, userSignupState }: SignupProps) => {
         <Box marginHorizontal="l">
           <Box>
             <TextField
-              onChangeText={handleChange("fName")}
-              placeholder="Full Name"
-              onBlur={handleBlur("fName")}
-              error={errors.fName}
-              touched={touched.fName}
+              onChangeText={handleChange("firstName")}
+              placeholder="First Name"
+              onBlur={handleBlur("firstName")}
+              error={errors.firstName}
+              touched={touched.firstName}
             />
           </Box>
+
+          <Picker
+            selectedValue={values.lastName}
+            onValueChange={(itemValue, itemIndex) =>
+              setFieldValue("lastName", itemValue)
+            }
+          >
+            {lastNameList.map((item, i) => {
+              return (
+                <Picker.Item
+                  key={item.id}
+                  label={item.lName}
+                  value={item.lName}
+                />
+              );
+            })}
+          </Picker>
+
+          {/* <Box>
+            <TextField
+              onChangeText={handleChange("lastName")}
+              placeholder="Last Name"
+              onBlur={handleBlur("lastName")}
+              error={errors.lastName}
+              touched={touched.lastName}
+            />
+          </Box> */}
           <Box>
             <TextField
               placeholder="Email"
