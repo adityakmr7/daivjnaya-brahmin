67d8c0b78e8c387117874b18d8e5d0b76bb28b20
Aditya kumar <adityakmr9672@gmail.com>
2020-11-30 17:21:02 +0530

    dynamic date added in metal api

--- src/services/metalPriceServices.ts
+++ src/services/metalPriceServices.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosRequestConfig } from "axios";
-
+import moment from "moment";
 class metalPriceServices {
   access_key = "j4sd9wo4brzcqq6z96p5topf1qr7ouww7794r45mk2sdmv6yd12s87f9wn9q";
   base = "INR";
   symbols = "XAU,XAG";
-  date = "2020-11-30";
+  date = moment().format("YY-MM-DD");
   url = `https://metals-api.com/api/${this.date}
     ?access_key=${this.access_key}&base=${this.base}&symbols=${this.symbols}`;
 
