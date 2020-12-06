c81110215eb1858b2d0e604402a7b42951b4f12c
ronakreact <ronakreact.iihglobal@gmail.com>
2020-12-06 21:49:33 +0530

    clean up

--- src/Screens/MyProfile/Videos.tsx
+++ src/Screens/MyProfile/Videos.tsx
@@ -6,30 +6,30 @@ import { ScrollView } from "react-native-gesture-handler";
 
 import { VideoCard } from "./components";
 
-export const VideoPost = [
-  {
-    id: 1,
-    user: "Siddharth Revankar",
-    userImage: require("../../../assets/images/ak.png"),
-    date: "17 Jun 2019",
-    caption: "Caption here",
-    image: require("../../../assets/images/post.png"),
-    comments: 10,
-    likes: 25,
-    video: require("../../../assets/images/video.mp4"),
-  },
-  {
-    id: 2,
-    user: "Siddharth Revankar",
-    userImage: require("../../../assets/images/pa.png"),
-    date: "17 Jun 2019",
-    caption: "Caption here",
-    image: require("../../../assets/images/post.png"),
-    comments: 10,
-    likes: 25,
-    video: require("../../../assets/images/video.mp4"),
-  },
-];
+// export const VideoPost = [
+//   {
+//     id: 1,
+//     user: "Siddharth Revankar",
+//     userImage: require("../../../assets/images/ak.png"),
+//     date: "17 Jun 2019",
+//     caption: "Caption here",
+//     image: require("../../../assets/images/post.png"),
+//     comments: 10,
+//     likes: 25,
+//     video: require("../../../assets/images/video.mp4"),
+//   },
+//   {
+//     id: 2,
+//     user: "Siddharth Revankar",
+//     userImage: require("../../../assets/images/pa.png"),
+//     date: "17 Jun 2019",
+//     caption: "Caption here",
+//     image: require("../../../assets/images/post.png"),
+//     comments: 10,
+//     likes: 25,
+//     video: require("../../../assets/images/video.mp4"),
+//   },
+// ];
 
 const Videos = ({ navigation }) => {
   const src = require("../../../assets/images/post.png");
@@ -40,7 +40,7 @@ const Videos = ({ navigation }) => {
           if (post.video) {
             return (
               <Box key={i}>
-                <VideoCard {...{ post }} />
+                {/* <VideoCard {...{ post }} /> */}
                 <Box height={5} backgroundColor="mainBackground" />
               </Box>
             );
