12975fe437c4cb9d15195978bedce9db6951f81d
adityakmr7 <adityakmr9672@gmail.com>
2020-12-20 20:45:19 +0530

    detail screens added

--- src/Screens/B2B/B2BProductDetail.tsx
+++ src/Screens/B2B/B2BProductDetail.tsx
@@ -1,22 +1,131 @@
 import React, { useEffect } from "react";
+import { Image } from "react-native";
 import { connect } from "react-redux";
 import { Box, Text } from "../../components";
 import { getB2bProductDetail } from "../../actions/b2bActions";
+import { ActivityIndicator, Dimensions } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 interface B2BDetailProps {
   route: any;
   getDetail: (id: number) => void;
   product: any;
 }
+const { width: wWidth, height: wHeight } = Dimensions.get("window");
+// vendor
+// creationDate: 1608451535000
+// designation: "Sales Manager"
+// email: "ocean@gmail.com"
+// fullName: "Shiva Revankar"
+// phoneNumber: "983412341"
+// place: "Karwar"
+// profilePic: null
+// pvId: 1
+// updatedDate: 1608451535000
+
 const B2BDetail = ({ route, getDetail, product }: B2BDetailProps) => {
   const { id } = route.params;
-  const {} = product;
+  const {
+    productDetailLoading,
+    productDetailData,
+    productDetailError,
+  } = product;
   useEffect(() => {
     getDetail(id);
   }, [id]);
+  const {
+    galleries,
+    email,
+    vendor,
+    phoneNumber,
+    productName,
+    updatedDate,
+    address,
+    creationDate,
+  } = productDetailData;
+  console.log("#######", productDetailData);
   return (
-    <Box>
-      <Text>ProductDetail {id}</Text>
+    <Box flex={1}>
+      <ScrollView>
+        {productDetailLoading ? (
+          <Box>
+            <ActivityIndicator />
+          </Box>
+        ) : (
+          <Box flex={1}>
+            {galleries !== undefined ? (
+              <Box>
+                <ScrollView
+                  horizontal={true}
+                  showsHorizontalScrollIndicator={false}
+                >
+                  {galleries.map((item: any, i: number) => {
+                    return (
+                      <Box key={item.pgId} height={wWidth + 100} width={wWidth}>
+                        <Image
+                          style={{
+                            width: "100%",
+                            height: "100%",
+                          }}
+                          source={{
+                            uri:
+                              item &&
+                              item._links &&
+                              item._links.image &&
+                              item._links.image.href &&
+                              item._links.image.href,
+                          }}
+                        />
+                      </Box>
+                    );
+                  })}
+                </ScrollView>
+              </Box>
+            ) : (
+              <Box>
+                <Text>No Image To Show</Text>
+              </Box>
+            )}
+            <Box marginHorizontal="s" marginVertical="s">
+              <Box>
+                <Text variant="cardTitle" color="primaryText">
+                  {productName}
+                </Text>
+                <Text>Address : {address ? address : null}</Text>
+                <Text>Email : {email ? email : null}</Text>
+                <Text>Phone : {phoneNumber ? phoneNumber : null}</Text>
+              </Box>
+              {vendor ? (
+                <Box marginVertical="s">
+                  <Text variant="cardTitle" color="primaryText">
+                    Vendor
+                  </Text>
+                  <Box
+                    marginVertical="s"
+                    width={wWidth}
+                    height={wWidth * 0.2}
+                    flexDirection="row"
+                    alignItems="center"
+                  >
+                    <Box width={"20%"} height={"100%"}>
+                      {vendor._links !== undefined ? (
+                        <Image
+                          style={{ width: "100%", height: "100%" }}
+                          source={{ uri: vendor._links.profilePic.href }}
+                        />
+                      ) : null}
+                    </Box>
+                    <Box marginHorizontal="s">
+                      <Text>{vendor.fullName}</Text>
+                      <Text>{vendor.designation}</Text>
+                    </Box>
+                  </Box>
+                </Box>
+              ) : null}
+            </Box>
+          </Box>
+        )}
+      </ScrollView>
     </Box>
   );
 };
--- src/Screens/B2B/B2BPropertyDetail.tsx
+++ src/Screens/B2B/B2BPropertyDetail.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { ActivityIndicator, Dimensions, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import { connect } from "react-redux";
 import { getB2bPropertyDetail } from "../../actions/b2bActions";
 import { Box, Text } from "../../components";
@@ -8,6 +10,7 @@ interface B2BPropertyDetailProps {
   getPropertyDetail: (id: number) => void;
   property: any;
 }
+const { width: wWidth, height: wHeight } = Dimensions.get("window");
 const B2BPropertyDetail = ({
   route,
   getPropertyDetail,
@@ -17,10 +20,113 @@ const B2BPropertyDetail = ({
   useEffect(() => {
     getPropertyDetail(id);
   }, [id]);
-  const {} = property;
+  const {
+    propertyDetailLoading,
+    propertyDetailData,
+    propertyDetailError,
+  } = property;
+  const {
+    galleries,
+    address,
+    creationDate,
+    email,
+    owner,
+    phoneNumber,
+    propertyName,
+    updatedDate,
+  } = propertyDetailData;
+  console.log("productDetailData", propertyDetailData);
+
+  //   creationDate: 1608459064000
+  // designation: "hdjdkccn"
+  // email: "adiaty@gmail.com"
+  // fullName: "aditya kumar"
+  // phoneNumber: "9985225252"
+  // place: "fjfmfncncn"
+  // poId: 1
+  // profilePic: null
+  // updatedDate: 1608459064000
+
   return (
-    <Box>
-      <Text>B2b Property Detail {id}</Text>
+    <Box flex={1}>
+      {propertyDetailLoading ? (
+        <Box>
+          <ActivityIndicator />
+        </Box>
+      ) : (
+        <Box flex={1}>
+          {galleries !== undefined ? (
+            <Box>
+              <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+              >
+                {galleries.map((item: any, i: number) => {
+                  return (
+                    <Box key={item.pgId} height={wWidth + 100} width={wWidth}>
+                      <Image
+                        style={{
+                          width: "100%",
+                          height: "100%",
+                        }}
+                        source={{
+                          uri:
+                            item &&
+                            item._links &&
+                            item._links.image &&
+                            item._links.image.href &&
+                            item._links.image.href,
+                        }}
+                      />
+                    </Box>
+                  );
+                })}
+              </ScrollView>
+            </Box>
+          ) : (
+            <Box>
+              <Text>No Image To Show</Text>
+            </Box>
+          )}
+          <Box>
+            <Box>
+              <Text variant="cardTitle" color="primaryText">
+                {propertyName}
+              </Text>
+              <Text>Address : {address ? address : null}</Text>
+              <Text>Email : {email ? email : null}</Text>
+              <Text>Phone : {phoneNumber ? phoneNumber : null}</Text>
+            </Box>
+            {owner ? (
+              <Box marginVertical="s">
+                <Text variant="cardTitle" color="primaryText">
+                  Vendor
+                </Text>
+                <Box
+                  marginVertical="s"
+                  width={wWidth}
+                  height={wWidth * 0.2}
+                  flexDirection="row"
+                  alignItems="center"
+                >
+                  <Box width={"20%"} height={"100%"}>
+                    {owner._links !== undefined ? (
+                      <Image
+                        style={{ width: "100%", height: "100%" }}
+                        source={{ uri: owner._links.profilePic.href }}
+                      />
+                    ) : null}
+                  </Box>
+                  <Box marginHorizontal="s">
+                    <Text>{owner.fullName}</Text>
+                    <Text>{owner.designation}</Text>
+                  </Box>
+                </Box>
+              </Box>
+            ) : null}
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 };
