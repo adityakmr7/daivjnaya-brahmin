30616e2821875b83dc66a432dc8bea57c23c214e
adityakmr007 <aditya.iihglobal@gmail.com>
2020-12-23 21:05:53 +0530

    jewellery detail actions added

--- src/Screens/Jewellery/ShopDetail.tsx
+++ src/Screens/Jewellery/ShopDetail.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getJewelleryShopDetail } from "../../actions/jewelleryActions";
 import { Box, Text } from "../../components";
 
-interface ShopDetailProps {}
-const ShopDetail = ({}: ShopDetailProps) => {
+interface ShopDetailProps {
+  route: any;
+  getShopDetail: (id: number) => void;
+  shop: any;
+}
+const ShopDetail = ({ route, getShopDetail, shop }: ShopDetailProps) => {
+  const { id, title } = route.params;
+  useEffect(() => {
+    getShopDetail(id);
+  }, [id]);
+  const { shopDetailLoading, shopDetailData, shopDetailError } = shop;
   return (
     <Box>
       <Text>ShopDetail</Text>
     </Box>
   );
 };
+function mapStateToProps(state: any) {
+  return {
+    shop: state.jewellery,
+  };
+}
+const mapDispatchToProps = (dispatch: any) => ({
+  getShopDetail: (id: number) => dispatch(getJewelleryShopDetail(id)),
+});
 
-export default ShopDetail;
+export default connect(mapStateToProps, mapDispatchToProps)(ShopDetail);
--- src/Screens/Jewellery/VendorDetail.tsx
+++ src/Screens/Jewellery/VendorDetail.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getJewelleryWorkerDetail } from "../../actions/jewelleryActions";
 import { Box, Text } from "../../components";
 
-interface ShopDetailProps {}
-const VendorDetail = ({}: ShopDetailProps) => {
+interface ShopDetailProps {
+  route: any;
+  getVendorDetail: (id: number) => void;
+  vendor: any;
+}
+const VendorDetail = ({ route, getVendorDetail, vendor }: ShopDetailProps) => {
+  const { id, title } = route.params;
+  useEffect(() => {
+    getVendorDetail(id);
+  }, [id]);
+  const { vendorDetailLoading, vendorDetailData, vendorDetailError } = vendor;
   return (
     <Box>
       <Text>VendorDetail</Text>
     </Box>
   );
 };
+function mapStateToProps(state: any) {
+  return {
+    vendor: state.jewellery,
+  };
+}
+const mapDispatchToProps = (dispatch: any) => ({
+  getVendorDetail: (id: number) => dispatch(getJewelleryWorkerDetail(id)),
+});
 
-export default VendorDetail;
+export default connect(mapStateToProps, mapDispatchToProps)(VendorDetail);
